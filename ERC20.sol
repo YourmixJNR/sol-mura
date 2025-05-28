5e5317ac080d34aab62a79f06273bb35302e7426
YourmixJNR <radmakson@gmail.com>
2025-05-28 14:13:06 +0100

    refactor: clean ups and ensuring event emissions

--- ERC20.sol
+++ ERC20.sol
@@ -2,6 +2,14 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract ERC20 {
+    // envents
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
     string public constant name = "MyTokenName";
     string public constant symbol = "MTN";
     uint8 public constant decimal = 18;
@@ -13,14 +21,6 @@ contract ERC20 {
     mapping(address => mapping(address => uint256)) public allowance;
 
     function transfer(address to, uint256 value) external returns (bool) {
-        // msg.sender: decrease token balance by value
-        // balanceOf[msg.sender] -= value;
-
-        // to: increase token balance by value
-        // balanceOf[to] += value;
-
-        // return true;
-
         _transfer(msg.sender, to, value);
     }
 
@@ -40,6 +40,8 @@ contract ERC20 {
 
         allowance[from][msg.sender] -= value;
 
+        emit Approval(from, msg.sender, allowance[from][msg.sender]);
+
         _transfer(from, to, value);
     }
 
@@ -50,6 +52,8 @@ contract ERC20 {
     ) private returns (bool) {
         require(balanceOf[from] >= value, "ERC20: Insufficient sender balance");
 
+        emit Transfer(from, to, value);
+
         balanceOf[from] -= value;
 
         balanceOf[to] += value;
@@ -59,6 +63,8 @@ contract ERC20 {
 
     function approve(address spender, uint256 value) external returns (bool) {
         allowance[msg.sender][spender] += value;
+
+        emit Approval(msg.sender, spender, value);
 
         return true;
     }
