1120a2055d1fc3c0151b2c9a588557852f60093e
YourmixJNR <radmakson@gmail.com>
2025-05-28 19:03:43 +0100

    refactor: update constructor to accept owner address and implement mint/burn functions

--- ERC20.sol
+++ ERC20.sol
@@ -13,25 +13,53 @@ contract ERC20 {
     string public name;
     string public symbol;
     uint8 public immutable decimal;
-
     uint256 public totalSupply;
+
+    address public owner;
 
     mapping(address => uint256) public balanceOf;
 
     mapping(address => mapping(address => uint256)) public allowance;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
+    constructor(
+        address _owner,
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimal
+    ) {
         name = _name;
         symbol = _symbol;
         decimal = _decimal;
+
+        owner = _owner;
     }
 
     function transfer(address to, uint256 value) external returns (bool) {
         _transfer(msg.sender, to, value);
     }
 
-    function giveMeOneToken() external {
-        balanceOf[msg.sender] += 1e18;
+    function _mint(address to, uint256 value) private {
+        balanceOf[to] += value;
+        totalSupply += value;
+    }
+
+    function mint(address to, uint256 value) external returns (bool) {
+        require(msg.sender == owner, "ERC20: Only owner can mint");
+        _mint(to, value);
+
+        emit Transfer(address(0), to, value);
+    }
+
+    function _burn(address from, uint256 value) private {
+        balanceOf[from] -= value;
+        totalSupply -= value;
+    }
+
+    function burn(address from, uint256 value) external returns (bool) {
+        require(msg.sender == owner, "ERC20: Only owner can burn");
+        _burn(from, value);
+
+        emit Transfer(from, address(0), value);
     }
 
     function transferFrom(
