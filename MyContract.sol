877064500fc965ca905677f6e231954d39c3e262
YourmixJNR <radmakson@gmail.com>
2025-06-04 18:56:54 +0100

    refactor: rename Parent contract to Father and add Mother contract for improved structure

--- MyContract.sol
+++ MyContract.sol
@@ -1,23 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.2 <0.9.0;
 
-contract Parent {
-    uint256 public myNumber;
+contract Father {
+    uint256 private myNumber;
 
-    constructor() {
-        myNumber = 40;
+    function setMyNumber(uint256 newNumber) external virtual {
+        myNumber = newNumber;
     }
-    function setMyNumber(uint256 newNumber) external {
+}
+contract Mother {
+    uint256 private myNumber;
+
+    function setMyNumber(uint256 newNumber) external virtual {
         myNumber = newNumber;
     }
 }
 
-contract Child is Parent {
-    constructor() {
-        // this will override the parent's constructor value
-        myNumber = 20;
-    }
+contract Child is Father, Mother {
+    uint256 private myNumber;
+
     function addToMyNumber(uint256 addition) external {
         myNumber += addition;
+    }
+
+    function setMyNumber(uint256 newNumber) external override(Father, Mother) {
+        myNumber = newNumber;
     }
 }
